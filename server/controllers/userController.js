f503eb7fc098087fa0e04f31a5d2a1f627d123bd
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-06-02 14:37:38 +0530

    Todo

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,20 @@
-// const express = require("express");
-// const userModel = require("../models/userModel");
+const express = require("express");
+const userModel = require("../models/userModel");
 
-// const homeP = async (req, res) => {
-//   try {
-//     res.send("HOme Page");
-//   } catch (e) {
-//     res.send("Error Page");
-//   }
-// };
+const homeP = async (req, res) => {
+  try {
+    res.send("HOme Page");
+  } catch (e) {
+    res.send("Error Page");
+  }
+};
 
-// const reqisterUser = async (req, res) => {
-//   try {
-//     const { name, userName, password } = req.body;
-//     const user = await userModel({ name, userName, password });
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-// module.exports = { reqisterUser, homeP };
+const reqisterUser = async (req, res) => {
+  try {
+    const { name, userName, password } = req.body;
+    const user = await userModel({ name, userName, password });
+  } catch (e) {
+    console.log(e);
+  }
+};
+module.exports = { reqisterUser, homeP };
